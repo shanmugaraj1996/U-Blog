3ef284e0e2e715c9e2a7ae2166b495dc4f9c1b53
Shanmugaraj <shanmugarajssp@gmail.com>
2021-01-20 00:01:00 +0530

    Checkpoint-1

--- src/com/upgrad/ublog/dtos/User.java
+++ src/com/upgrad/ublog/dtos/User.java
@@ -14,26 +14,66 @@ package com.upgrad.ublog.dtos;
 
 public class User {
 
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "userId=" + userId +
-//                ", emailId='" + emailId + '\'' +
-//                ", password='" + password + '\'' +
-//                '}';
-//    }
 
-//    public static void main(String[] args) {
-//        User user = new User();
-//        user.setUserId(1);
-//        user.setEmailId("dummy@dummy.com");
-//        user.setPassword("password");
-//
-//        System.out.println(user);
-//
+    private int userId;
+    private String emailId;
+    private String password;
+
+    public User(int userId, String emailId, String password) {
+        this.userId = userId;
+        this.emailId = emailId;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", emailId='" + emailId + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setUserId(1);
+        user.setEmailId("dummy@dummy.com");
+        user.setPassword("password");
+
+        System.out.println(user);
+
 //        /**
 //         * Your output should be similar to this.
 //         * User{userId=1, emailId='dummy@dummy.com', password='password'}
 //         */
-//    }
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
